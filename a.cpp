4839bdba817e8e851ac14c66384b2b94bd43eda9
amd <anvi.deguzman@gmail.com>
2025-01-15 14:11:56 +0800

    secondary

--- a.cpp
+++ a.cpp
@@ -3,7 +3,36 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Good Day\n");
+    int a;
+    int b;
+
+    printf("The Multitable program\n\n");
+
+    printf("\t");
+
+    for(a=1; a<=10; a++)
+    {
+        printf("%i\t", a);
+    };
+
+    printf("\n");
+
+    for(a=1; a<=10; a++)
+    {
+        printf("%i\t", a);
+        
+        for(b=1; b<=10; b++)
+        {
+            printf("%i\t", a*b);
+
+        };
+
+        printf("\n");
+
+
+    };
+
+    printf("Program End...\n\n");
 
 
     return 0;
