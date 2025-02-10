42e69c9680363ed864c451e52d86beebfa1dd905
amd <anvi.deguzman@gmail.com>
2025-02-10 14:32:38 +0800

    stage01

--- src/a.cpp
+++ src/a.cpp
@@ -6,32 +6,33 @@ int main(int argc, char *argv[])
     int a;
     int b;
 
-    if(argc != 2)
+    if(argc == 3)
     {
-        printf("ERROR: Input X and Y in args...\n\n");
-    };
+        printf("\n");
 
-    printf("\n");
-
-    printf("The Mutiplication Table Program\n");
-    printf("C/C++ Test Application for Unix/Linux\n");
-    printf("Not for Commercial, Scientific and Military Use...\n\n");
+        printf("The Mutiplication Table Program\n");
+        printf("C/C++ Test Application for Unix/Linux\n");
+        printf("Not for Commercial, Scientific and Military Use...\n\n");
 
 
-    for(a=1; a<=10; a++)
-    {
-        
-        for(b=1; b<=10; b++)
+        for(a=1; a<=10; a++)
         {
-            printf("%i\t", a*b);
+        
+            for(b=1; b<=10; b++)
+            {
+                printf("%i\t", a*b);
+
+            };
+
+            printf("\n");
+
 
         };
 
-        printf("\n");
-
 
     };
 
+    
     printf("\n");
 
     printf("Program End...\n\n\n");
